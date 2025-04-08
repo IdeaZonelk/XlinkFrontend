dd8b461054276eaabae7801755d9d2283eaf78bc
Dashini Abeynayake <127436583+DashiniA@users.noreply.github.com>
2025-04-08 10:51:32 +0530

    Update posBillCalculation.jsx

--- src/component/pos/components/posBillCalculation.jsx
+++ src/component/pos/components/posBillCalculation.jsx
@@ -752,7 +752,7 @@ const BillingSection = ({ productBillingHandling, setProductBillingHandling, set
                                     onClick={handleDiscountAccess}
                                     className="submit w-1/2 mr-2 text-white px-4 py-2 rounded-lg shadow-md hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-blue-400"
                                 >
-                                    loging
+                                    logging
                                 </button>
                                 <button
                                     type="button"
