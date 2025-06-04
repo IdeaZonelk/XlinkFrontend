2f69f150f21e12a50af87201fe182f2c2c69def2
Dashini Abeynayake <dashiniabeynayake0718@gmail.com>
2025-06-04 15:53:41 +0530

    license

--- src/component/sales/CreateSale.jsx
+++ src/component/sales/CreateSale.jsx
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2025 Ideazone (Pvt) Ltd
+ * Proprietary and Confidential
+ *
+ * This source code is part of a proprietary Point-of-Sale (POS) system developed by Ideazone (Pvt) Ltd.
+ * Use of this code is governed by a license agreement and an NDA.
+ * Unauthorized use, modification, distribution, or reverse engineering is strictly prohibited.
+ *
+ * Contact info@ideazone.lk for more information.
+ */
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { handleProductSelect, handleProductSearch, handleCustomerSearch, handleCustomerSelect, handleWarehouseChange, handleVariationChange, getProductCost, getDiscount, getQty, getPriceRange, handleDelete, handleQtyChange, getTax, handleSave } from './SaleController'
